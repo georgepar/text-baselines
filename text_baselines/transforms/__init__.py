9950b8dd31c9f7ef79ace94b146fc755f58e0b10
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2021-02-25 14:19:30 +0200

    Move code from https://github.com/georgepar/slp

---
+++ text_baselines/transforms/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from text_baselines.transforms.util import SklIdentityTransformer, SklComposer
---
+++ text_baselines/transforms/text/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from text_baselines.transforms.text.skl import (
+    Untokenizer,
+    PunctuationStripper,
+    SplitTokenizer,
+    NltkTokenizer,
+    SpacyTokenizer,
+    NBOWVectorizer,
+)
